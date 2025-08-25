75fba2c2305ce42f0e866a16cb2a7fe8bdc17a85
jknigel <ajknigel360@gmail.com>
2025-08-25 18:38:32 +0800

    Update 2.0 Gradio AI Apps

---
+++ gradio-image-classification.py
@@ -0,0 +1,23 @@
+import torch
+
+model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
+
+import requests
+from PIL import Image
+from torchvision import transforms
+
+# Download human-readable labels for ImageNet.
+response = requests.get("https://git.io/JJkYN")
+labels = response.text.split("\n")
+def predict(inp):
+ inp = transforms.ToTensor()(inp).unsqueeze(0)
+ with torch.no_grad():
+  prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+  confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
+ return confidences
+
+import gradio as gr
+gr.Interface(fn=predict,
+       inputs=gr.Image(type="pil"),
+       outputs=gr.Label(num_top_classes=3),
+       examples=[r"C:\Users\nigel\OneDrive\Desktop\IBM Agentic AI Professional Certificate\Course 2 - Building RAG AI App\images\lion.jpeg", r"C:\Users\nigel\OneDrive\Desktop\IBM Agentic AI Professional Certificate\Course 2 - Building RAG AI App\images\cheetah.jpg"]).launch()
